dd0f1128a3f03d3c5a959811ea55abc474b8d206
navarang <navarang614@gmail.com>
2025-06-20 15:45:49 +0530

    Update database connection string

--- Program.cs
+++ Program.cs
@@ -1,51 +1,3 @@
-//using Microsoft.EntityFrameworkCore;
-//using EmployeeManagementAPI.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Configure services
-//builder.Services.AddControllers();
-
-//// ? Add CORS policy
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAngularApp", policy =>
-//    {
-//        policy.WithOrigins("http://localhost:4200")
-//              .AllowAnyHeader()
-//              .AllowAnyMethod();
-//    });
-//});
-
-//// Add Swagger/OpenAPI
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//// Configure EF Core to use SQL Server
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-//var app = builder.Build();
-
-//// Configure middleware
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//// ? Use CORS policy before Authorization
-//app.UseCors("AllowAngularApp");
-
-//app.UseAuthorization();
-
-//// Map controller routes
-//app.MapControllers();
-
-//app.Run();
-
 using Microsoft.EntityFrameworkCore;
 using EmployeeManagementAPI.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -131,12 +83,7 @@ app.UseCors("AllowAngularApp");
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
-//New additions for chathub
-//app.UseCors("CorsPolicy");
-
 app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
